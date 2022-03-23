95fce69f8a718bd686a36fce8bdcfdd332a2bd23
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-03-23 04:00:35 -0400

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,16 @@
+class Team:
+    name = "teamName"
+    city = "teamCity"
+    roster = ["player1", "player2", "player3"]
+    year_founded = 0
+    coach = "coachName"
+
+class Warriors(Team):
+    name = "Golden State Warriors"
+    sport = "basketball"
+    playing_site = "Oracle Arena"
+
+class Buccaneers(Team):
+    name = "Tampa Bay Buccaneers"
+    championships = 2
+    contact_sport = True
