6bb1574e2fd33e67054d94891c99c775a4f17ecb
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-04-16 18:04:21 -0400

    Edited Checkdates function

--- file_transfer_func.py
+++ file_transfer_func.py
@@ -23,8 +23,8 @@ def checkdates(self):
     files = os.listdir(source)  #defining "files" as the directory list of files in the source folder 
     # for loop function that will execute following script for each item or file " i " in the directory list of files in the source folder 
     for i in files:
-        modified_time = (os.path.getmtime('/Users/User/OneDrive/Desktop/Modified or New/{}'.format(i)))
-        created_time = (os.path.getctime('/Users/User/OneDrive/Desktop/Modified or New/{}'.format(i)))
+        modified_time = (os.path.getmtime(source+i))
+        created_time = (os.path.getctime(source+i))
         #string decribing when file " i " in the folder was modified, created
         a = "Last modified: %s" % time.ctime(modified_time)
         b = "Created: %s" % time.ctime(created_time)
@@ -34,24 +34,25 @@ def checkdates(self):
         #Determining whether to use modified_time or created_time based on whether or not file has been modified
         if f == e:
             d=f
-        if  f != e:
+        elif  f != e:
             d=e
         #getting current time since epoch (seconds) and subtracting modified/created time of files (seconds) to get
         #time since creation/modification in seconds, "sime_since_modcreation"
         date_time = int(time.time())
-        time_since_modcreation = date_time - d                
-        print(i + '\n' + b + ' || ' +a + '\n' +'{} - {} = {}.        {} Was made/edited {} Seconds Ago.'.format(date_time,d,time_since_modcreation,i,time_since_modcreation))
-        #setting condition to move files that have been created or modified within the last day (86400 seconds)
-        if (time_since_modcreation) > 86400: 
-            print('{} has been modified or created more than a day ago.'.format(i))
-            print('{}  > 86400.'.format(time_since_modcreation))
-            print(' NOT MOVED: REMAINS IN MODIIFIED OR NEW\n- - - - - - - - - -\n\n')
+        time_since_modcreation = date_time - d
+        modTime = datetime.datetime.fromtimestamp(modified_time)
+        twentyFour = datetime.datetime.now() - datetime.timedelta(hours=24)
+        print(i + '\n' + b + ' || ' +a + '\n')
+        #setting condition to move files that have been created or modified within the last day
+        if (modTime > twentyFour):
+            print(' MOVED: SENT TO FILE DESTINATION\n- - - - - - - - - -\n\n')
+            #moving files that were modified in the last 24 hours
+            shutil.move(source+i, destination)            
             continue # using continue to move on to next item " i " in file if condition is satisfied
-        print(' MOVED: SENT TO FILE DESTINATION\n- - - - - - - - - -\n\n')
-        #moving files that did not satisfy the if condition immediately above
-        shutil.move(source+i, destination)
-        #shutil.copy(source+i, destination)
-    
+        else:
+            print('{} has been modified or created more than a day ago.'.format(i))            
+            print(' NOT MOVED: REMAINS IN MODIIFIED OR NEW\n- - - - - - - - - -\n\n')               
+           
 def close(self):
     self.master.destroy()
     
