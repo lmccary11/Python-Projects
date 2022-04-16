5230ee1910117cb5f5fa92e4d8ea7dd8f6cbbcf7
Lawrence McCary <98392880+lmccary11@users.noreply.github.com>
2022-04-16 08:14:28 -0400

    Update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -1,3 +1,25 @@
+class User:
+    name = "No Name Provided"
+    email = ""
+    password = "1234abcd"
+    account = 0
+
+    def login(self):
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your password: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}".format(self.name))
+        else:
+            print("You are not authorized for ths page.")
+new_user = User()
+new_user.login()
+
+def __init__(self, name, email, password, account):
+    self.name = name
+    self.email = email
+    self.password = password
+    self.account = account
+
 class Team:
     name = "teamName"
     city = "teamCity"
